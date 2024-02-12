de4883487fe409286c0bc1a083190d1cad6b5844
Natasha England-Elbro <natasha@natashaee.me>
2024-02-12 21:19:27 +0000

    chore: add more UB tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use std::{marker::PhantomPinned, mem::MaybeUninit, pin::Pin};
+use std::{marker::PhantomPinned, pin::Pin};
 
 use iter::{Iter, IterMut};
 
@@ -58,17 +58,59 @@ impl<T: Unpin, const SIZE: usize> Unpin for PinArray<T, SIZE> {}
 
 #[cfg(test)]
 mod tests {
+    use std::{
+        marker::{PhantomData, PhantomPinned},
+        pin::Pin,
+    };
+
     use crate::PinArray;
+
+    #[derive(Clone, Copy, Debug, Default, Eq)]
+    struct NotUnpin {
+        _p: PhantomPinned,
+        v: u8,
+    }
+
+    impl NotUnpin {
+        fn new(v: u8) -> Self {
+            Self {
+                _p: PhantomPinned,
+                v,
+            }
+        }
+    }
+    impl PartialEq for NotUnpin {
+        fn eq(&self, other: &Self) -> bool {
+            self.v == other.v
+        }
+    }
+
+    #[track_caller]
+    fn mut_iter_test<T: Copy + Eq + core::fmt::Debug, const SZ: usize>(mut els: [T; SZ]) {
+        let mut p = core::pin::pin!(unsafe { PinArray::new_unchecked(els) });
+        let iter = p.as_mut().iter_mut();
+        let vs = iter.collect::<Vec<_>>();
+        assert_eq!(
+            vs,
+            els.iter_mut()
+                .map(|e| unsafe { Pin::new_unchecked(e) })
+                .collect::<Vec<_>>()
+        );
+    }
 
     // this is mostly here to check that IterMut doesn't cause UB according to MIRI
     #[test]
     fn mut_iterator_multi_borrow_ub() {
-        let mut p = core::pin::pin!(PinArray::new([1, 2, 3, 4]));
-        let mut iter = p.as_mut().iter_mut();
-        let v1 = iter.next();
-        let v2 = iter.next();
-        assert_eq!(v1.map(|o| *o.get_mut()), Some(1));
-        assert_eq!(v2.map(|o| *o.get_mut()), Some(2));
+        mut_iter_test([1, 2, 3, 4]);
+    }
+    #[test]
+    fn mut_iter_needs_pin() {
+        mut_iter_test(core::array::from_fn::<_, 3, _>(|i| NotUnpin::new(i as u8)));
+    }
+
+    #[test]
+    fn mut_iter_zst() {
+        mut_iter_test([PhantomData::<()>, PhantomData, PhantomData]);
     }
 }
 
