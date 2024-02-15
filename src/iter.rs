9682c7d25e36e83f5d9ab5e70d7b650881a99213
Natasha England-Elbro <natasha@natashaee.me>
2024-02-15 13:47:56 +0000

    feat: make iterators fixed size

--- src/iter.rs
+++ src/iter.rs
@@ -2,6 +2,31 @@ use std::{marker::PhantomData, pin::Pin, ptr::NonNull};
 
 use crate::PinArray;
 
+macro_rules! impl_iter {
+    ($name:ident <$l:lifetime, $t:ident, $sz:ident> { type Item = $item:ty; idx = $i:ident; $me:ident => $get:expr }) => {
+        impl<$l, $t, const $sz: usize> ExactSizeIterator for $name<$l, $t, $sz> {}
+        impl<$l, $t, const $sz: usize> Iterator for $name<$l, $t, $sz> {
+            type Item = $item;
+
+            fn next(&mut self) -> Option<Self::Item> {
+                if self.$i >= $sz {
+                    None
+                } else {
+                    let $me = &self;
+                    let item = $get;
+                    self.i += 1;
+                    Some(item)
+                }
+            }
+            fn size_hint(&self) -> (usize, Option<usize>) {
+                debug_assert!(self.$i <= $sz);
+                let sz = $sz - self.$i;
+                (sz, Some(sz))
+            }
+        }
+    };
+}
+
 /// Iterator over references of a [`PinArray`]
 ///
 /// For more see [`PinArray::iter`]
@@ -9,17 +34,10 @@ pub struct Iter<'p, T, const SZ: usize> {
     pub(crate) i: usize,
     pub(crate) els: &'p PinArray<T, SZ>,
 }
-impl<'p, T, const SZ: usize> Iterator for Iter<'p, T, SZ> {
-    type Item = &'p T;
 
-    fn next(&mut self) -> Option<Self::Item> {
-        if self.i >= SZ {
-            None
-        } else {
-            let l = self.els.get(self.i).unwrap();
-            self.i += 1;
-            Some(l)
-        }
+impl<'p, T, const SZ: usize> Iter<'p, T, SZ> {
+    pub fn new(els: &'p PinArray<T, SZ>) -> Self {
+        Self { i: 0, els }
     }
 }
 
@@ -45,16 +63,37 @@ impl<'p, T, const SZ: usize> IterMut<'p, T, SZ> {
         }
     }
 }
-impl<'p, T, const SZ: usize> Iterator for IterMut<'p, T, SZ> {
-    type Item = Pin<&'p mut T>;
 
-    fn next(&mut self) -> Option<Self::Item> {
-        if self.i >= SZ {
-            None
-        } else {
-            let lp = unsafe { self.el_ptr.as_ptr().add(self.i) };
-            self.i += 1;
-            Some(unsafe { Pin::new_unchecked(lp.as_mut().unwrap()) })
-        }
+impl_iter!(Iter <'p, T, SZ> {
+    type Item = &'p T;
+    idx = i;
+    me => me.els.get(me.i).unwrap()
+
+});
+impl_iter!(IterMut <'p, T, SZ> {
+    type Item = Pin<&'p mut T>;
+    idx = i;
+    me => unsafe {
+        Pin::new_unchecked(me.el_ptr.as_ptr().add(me.i).as_mut().unwrap())
+    }
+
+});
+
+#[cfg(test)]
+mod tests {
+    use crate::PinArray;
+
+    use super::Iter;
+    #[test]
+    fn size_matches() {
+        let pa = PinArray::new([1, 2, 3]);
+        let mut i = Iter::new(&pa);
+        assert_eq!(i.len(), 3);
+        i.next();
+        assert_eq!(i.len(), 2);
+        i.next();
+        assert_eq!(i.len(), 1);
+        i.next();
+        assert_eq!(i.len(), 0);
     }
 }
